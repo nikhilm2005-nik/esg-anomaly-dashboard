04bf07064ec39ecaf4c04ad92539966cbd42d170
nikhilm2005-nik <nikhileducation2027@gmail.com>
2026-04-15 17:55:51 +0530

    updated the main server page wrt the changes made in threshold and upload controller

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const thresholdRoutes = require('./routes/thresholds');
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api/thresholds', thresholdRoutes);
 
 const companyRoutes = require('./routes/companies');
 const esgRoutes = require('./routes/esg');
